58f601817b9f70755043887252b417a5b9c6f40e
Dallin24 <the4gates24@gmail.com>
2022-04-29 13:42:53 -0600

    Completed Health Bar

--- src/game/view/GameInfo.java
+++ src/game/view/GameInfo.java
@@ -1,6 +1,7 @@
 package game.view;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.JComponent;
@@ -9,25 +10,35 @@ import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
+import javax.swing.table.TableCellRenderer;
 
 import game.model.Cell;
+
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+
 import java.awt.Font;
+import java.util.ArrayList;
 
 public class GameInfo extends JPanel
 {
 	private int cellWidth;
 	private int cellHeight;
-	
+
 	private JTable healthBar;
-	
+	private ArrayList<Boolean> healthData;
+
 	private Dimension normalDimension;
-	
+
+	private ImageIcon green;
+	private ImageIcon white;
+
 	private Cell player;
-	
+
 	private JTextField playerName;
 	private JTextField playerHealth;
-	
+
 	private SpringLayout layout;
 
 	public GameInfo(Cell player, int cellWidth, int cellHeight)
@@ -36,18 +47,27 @@ public class GameInfo extends JPanel
 
 		this.cellWidth = cellWidth;
 		this.cellHeight = cellHeight;
-		
+
 		this.normalDimension = new Dimension(380, 990);
-		
-		this.healthBar = new JTable(10, 1);
-		
+
+		this.healthBar = new JTable(10, 1)
+		{
+			public Class getColumnClass(int column)
+			{
+				return ImageIcon.class;
+			}
+		};
+		this.healthData = new ArrayList<Boolean>();
+
+		this.green = new ImageIcon(getClass().getResource("/game/view/images/Green.png"));
+		this.white = new ImageIcon(getClass().getResource("/game/view/images/White.png"));
+
 		this.player = player;
 		this.playerName = new JTextField();
 		this.playerHealth = new JTextField();
-		
+
 		this.layout = new SpringLayout();
 
-		
 
 		setupPanel();
 		setupLayout();
@@ -57,7 +77,7 @@ public class GameInfo extends JPanel
 	{
 		this.setLayout(layout);
 		this.setSize(normalDimension);
-		
+
 		playerName.setText(player.getPlayerName());
 		playerName.setFont(new Font("Kohinoor Gujarati", Font.BOLD, 54));
 		playerName.setHorizontalAlignment(JTextField.CENTER);
@@ -65,8 +85,8 @@ public class GameInfo extends JPanel
 		playerName.setHighlighter(null);
 		playerName.setBackground(this.getBackground());
 		playerName.setBorder(null);
-		
-		if(player.getPlayerName().equals("RED"))
+
+		if (player.getPlayerName().equals("RED"))
 		{
 			playerName.setForeground(Color.RED);
 		}
@@ -75,39 +95,71 @@ public class GameInfo extends JPanel
 			playerName.setForeground(Color.BLUE);
 		}
 
-		
 		playerHealth.setText(player.getHealth() + "");
-		
+
 		this.add(playerName);
+
+		playerHealth.setFont(new Font("Kohinoor Gujarati", Font.BOLD, 22));
+		playerHealth.setHorizontalAlignment(JTextField.CENTER);
+		playerHealth.setEditable(false);
+		playerHealth.setHighlighter(null);
+		playerHealth.setBackground(this.getBackground());
+		playerHealth.setBorder(null);
 		this.add(playerHealth);
-		
+
 		healthBar.setRowHeight(30);
-		healthBar.setSize(new Dimension(30 * 10, 38 * 1));
+		healthBar.setPreferredSize(new Dimension(cellWidth, cellHeight * 10));
+		healthBar.setSize(new Dimension(cellWidth, cellHeight * 10));
 		healthBar.setCellSelectionEnabled(false);
 		healthBar.setGridColor(Color.BLACK);
 		healthBar.setEnabled(false);
+		healthBar.setBackground(Color.BLACK);
+
+		for (int index = 0; index < 10; index++)
+		{
+			healthData.add(true);
+			healthBar.setValueAt(green, index, 0);
+		}
+
 		this.add(healthBar);
-		
-		
+
 	}
-	
+
 	private void setupLayout()
 	{
 		layout.putConstraint(SpringLayout.NORTH, playerName, cellHeight, SpringLayout.NORTH, this);
 		layout.putConstraint(SpringLayout.WEST, playerName, cellWidth, SpringLayout.WEST, this);
 		layout.putConstraint(SpringLayout.EAST, playerName, -cellWidth, SpringLayout.EAST, this);
+
+		layout.putConstraint(SpringLayout.NORTH, playerHealth, 0, SpringLayout.SOUTH, healthBar);
+		layout.putConstraint(SpringLayout.WEST, playerHealth, cellWidth * 3, SpringLayout.WEST, this);
 		
-		layout.putConstraint(SpringLayout.NORTH, playerHealth, 0, SpringLayout.SOUTH, playerName);
-		layout.putConstraint(SpringLayout.WEST, playerHealth, 0, SpringLayout.WEST, playerName);
-		
+		layout.putConstraint(SpringLayout.WEST, healthBar, cellWidth * 3, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.EAST, healthBar, -cellWidth * 3, SpringLayout.EAST, this);
+		layout.putConstraint(SpringLayout.NORTH, healthBar, cellHeight * 4, SpringLayout.NORTH, this);
+
 	}
-	
+
 	public void updatePlayerData(String type, int healthChange)
 	{
-		if(type.equals("HEALTH"))
+		if (type.equals("HEALTH"))
 		{
 			player.setHealth(player.getHealth() + healthChange);
 			playerHealth.setText(player.getHealth() + "");
+			healthData.remove(healthData.size() - 1);
+			
+			
+			for (int index = 0; index < 10; index++)
+			{
+				healthBar.setValueAt(green, index, 0);
+			}
+
+			
+			for (int index = 9 - healthData.size(); index >= 0; index--)
+			{
+				healthBar.setValueAt(white, index, 0);
+			}
 		}
 	}
+
 }
